0f29b63a9f417ae70fed67f923eab8864a70e59f
brito101 <rodrigobrito101@hotmail.com>
2024-03-16 19:04:10 -0300

    exercise: login

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,6 +1,26 @@
+import { cookies } from "next/headers";
 import Link from "next/link";
 
-export default function Menu() {
+type Account = {
+  autorizado: boolean;
+  usuario: string;
+};
+
+export default async function Menu() {
+  let account: Account = {
+    autorizado: false,
+    usuario: "",
+  };
+  const token = cookies().get("token")?.value;
+  const response = await fetch("https://api.origamid.online/conta/perfil", {
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  });
+  if (response.ok) {
+    account = (await response.json()) as Account;
+  }
+
   return (
     <ul className="menu">
       <li>
@@ -11,6 +31,13 @@ export default function Menu() {
       </li>
       <li>
         <Link href="/courses">Courses</Link>
+      </li>
+      <li>
+        {account.autorizado ? (
+          account.usuario
+        ) : (
+          <Link href="/login">Login</Link>
+        )}
       </li>
     </ul>
   );
